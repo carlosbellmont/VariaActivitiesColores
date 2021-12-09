c6ab21b01edd04fa277c3f05bbb11c61957405fd
Carlos <carlostena@centroafuera.onmicrosoft.com>
2021-12-09 14:05:35 +0100

    Initial commit

---
+++ app/src/main/java/com/carlostena/variaactivities/SecondActivity.kt
@@ -0,0 +1,25 @@
+package com.carlostena.variaactivities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.carlostena.variaactivities.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val initialData = intent.getIntExtra("NUM_ALEATORIO", 0)
+
+        binding.tvTexto.text = "$initialData"
+
+        binding.bNextActivity2.setOnClickListener {
+            LastActivity.launch(this, binding.tvTexto.text.toString())
+        }
+    }
+}
